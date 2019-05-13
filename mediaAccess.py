0c7963d81df83f1bd792d4a1d813bd8c70f97188
Ubuntu <ubuntu@gomphrena.cloud.compas.cs.stonybrook.edu>
2019-05-13 22:49:18 +0000

    fixed atempt to stop queueing cassandra

--- mediaAccess.py
+++ mediaAccess.py
@@ -90,7 +90,7 @@ class AddMedia(Resource):
 			#return make_response(jsonify(status="error", error="No filetype!"), 430)
 		id = generateNewMediaID()
 		#queueCassandraWrite(id, file, filetype)
-		session.execute("INSERT INTO images(id, contents, contenttype) VALUES (%s, %s, %s)", (id, image, imagetype))
+		session.execute("INSERT INTO images(id, contents, contenttype) VALUES (%s, %s, %s)", (id, file, filetype))
 		client = getMongoClient()
 		db = client["Project"]
 		media_col = db["media"]
